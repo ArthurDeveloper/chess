780e5ef4e79ab8aa6a59d0c7192cba970b951ed2
ArthurDev <arthurdevoficial@gmail.com>
2023-07-07 21:07:24 -0300

    Removed some dead code

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,6 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/Graphics/Color.hpp>
-#include <SFML/Graphics/RectangleShape.hpp>
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/Window/Event.hpp>
-#include <SFML/Window/Mouse.hpp>
-#include <SFML/Window/WindowStyle.hpp>
-#include <iostream>
 #include <cmath>
-#include <ostream>
-#include <vector>
 #include "game.h"
-
-// For debug purposes only
-/*void printBoard() {
-	system("clear");
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
-			if (board[i][j] == 0) {
-				std::cout << "  ";
-				continue;
-			}
-			std::cout << abs(board[i][j]) << " ";
-		}
-		std::cout << std::endl;
-	}
-}*/
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(64 << 3, 64 << 3), "Chess", sf::Style::Titlebar);
